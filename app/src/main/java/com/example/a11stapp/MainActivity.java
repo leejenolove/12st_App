5a6769b57db814caeb5d3a357604b84c131b695d
leejenolove <f22546@naver.com>
2022-06-11 14:02:52 +0900

    21173041 강민정 기말과제_rank, audiAcc, openDt 추가

--- app/src/main/java/com/example/a11stapp/MainActivity.java
+++ app/src/main/java/com/example/a11stapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.a11stapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
     static RequestQueue requestQueue;
 
+    RecyclerView recyclerView;
+    MovieAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestText = findViewById(R.id.requestText);
-        responseText = findViewById(R.id.responseText);
 
         Button requestBtn = findViewById(R.id.requestBtn);
         requestBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new MovieAdapter();
+        recyclerView.setAdapter(adapter);
     }
 
     public void makeRequest() {
@@ -77,16 +88,20 @@ public class MainActivity extends AppCompatActivity {
         println("요청보냄");
 
     }
-
-
     public void println(String data) {
-        responseText.append(data+"\n");
+        Log.d("MainActivity",data);
     }
     public void processResponse(String response)
     {
         Gson gson = new Gson();
         MovieList movieList = gson.fromJson(response, MovieList.class);
         println("영화 정보의 수 : " + movieList.boxOfficeResult.dailyBoxOfficeList.size());
+
+        for(int i = 0; i < movieList.boxOfficeResult.dailyBoxOfficeList.size();i++){
+            Movie movie = movieList.boxOfficeResult.dailyBoxOfficeList.get(i);
+            adapter.addItem(movie);
+        }
+        adapter.notifyDataSetChanged();
 
 }
 }
